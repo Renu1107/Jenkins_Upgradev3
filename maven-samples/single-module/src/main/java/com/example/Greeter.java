8a421dec5125f2d214d5157d495b4ea2e7931c14
Renu1107 <anushirenu@gmail.com>
2025-12-06 23:20:02 +0530

    Update Greeter.java

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method & good evening !!!");
+	  System.out.println("Hello from Main Method & good morning !!!");
   }
 	
 }
